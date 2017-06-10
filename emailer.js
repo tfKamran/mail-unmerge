6a3eaca7bf52fda7135192723a45fc70b386ab84
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-06-10 12:57:06 +0530

    Ask for configurations, if not configured already

--- emailer.js
+++ emailer.js
@@ -1,11 +1,18 @@
-var nodemailer = require('nodemailer');
-
-var configs = require("./configs.json");
+const nodemailer = require('nodemailer');
+const urlencode = require('urlencode');
+const configs = require('./config-manager').getConfiguration();
 
 module.exports = function(to, subject, body, attachments) {
     console.log("Preparing to send email...");
 
-    var transporter = nodemailer.createTransport(configs.email_server);
+    var transporter = nodemailer.createTransport(
+        configs.email_server.substring(0, configs.email_server.indexOf("://") + 3)
+        + urlencode(configs.email_id)
+        + ":"
+        + urlencode(configs.password)
+        + "@"
+        + configs.email_server.substring(configs.email_server.indexOf("://") + 3)
+        );
 
     var mailOptions = {
         from: '"' + configs.sender_name + '" <' + configs.email_id + '>',
