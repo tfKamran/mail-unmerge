e352256db7ee970f1028eac62d7e064868b75337
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-06-10 12:57:47 +0530

    Don't store passwords locally

--- config-manager.js
+++ config-manager.js
@@ -13,14 +13,16 @@ module.exports = {
 
         checkAndAskFor("email_id", "What source email address would you like to use? ");
         checkAndAskFor("sender_name", "What name would you like to reflect in the email? ");
-        checkAndAskFor("password", "Password for your email address: ", {hideEchoBack: true});
         checkAndAskFor("email_server", "What is your outgoing mail server address? ");
+        checkAndAskFor("password", "Password for your email address: ", {hideEchoBack: true});
 
         function checkAndAskFor(key, caption, options) {
             if (!configs[key]) {
                 configs[key] = readline.question(caption, options);
-                
-                fs.writeFileSync(configFile, JSON.stringify(configs), 'utf-8');
+
+                if (!options || !options.hideEchoBack) {
+                    fs.writeFileSync(configFile, JSON.stringify(configs), 'utf-8');
+                }
             }
         }
 
