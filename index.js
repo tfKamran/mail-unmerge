5d1be5eceb8be1d39b0aad5bfd349019a316997b
Kamran Ahmed Ansari <team.radiant@gmail.com>
2017-07-03 12:39:42 +0530

    Prompt to retry sending the failed emails, if any

--- index.js
+++ index.js
@@ -23,90 +23,98 @@ if (process.argv.length != 4) {
 const headerCSV = process.argv[2];
 const contentTemplate = process.argv[3];
 
-const emailBodyTemplate = fs.readFileSync(contentTemplate, 'utf-8').split("\n").join("<br />");
+intiateEmails(headerCSV, contentTemplate);
 
-const headerJSON = [];
+function intiateEmails(headerCSV, contentTemplate) {
+    const headerJSON = [];
 
-csv().fromFile(headerCSV)
-    .on('json', (item) => {
-        headerJSON.push(item);
-    })
-    .on('done', (error) => {
-        if (error) {
-            console.log("Error parsing CSV file");
+    const emailBodyTemplate = fs.readFileSync(contentTemplate, 'utf-8').split("\n").join("<br />");
+
+    csv().fromFile(headerCSV)
+        .on('json', (item) => {
+            headerJSON.push(item);
+        })
+        .on('done', (error) => {
+            if (error) {
+                console.log("Error parsing CSV file");
+            } else {
+                var batchEmail = [];
+
+                headerJSON.forEach(function(item, index, allItems) {
+                    var emailBody = emailBodyTemplate;
+
+                    var attachments = [];
+
+                    Object.keys(item).forEach(key => {
+                        emailBody = emailBody.split("<" + key + ">").join(item[key]);
+
+                        if (key.startsWith("attachment")) {
+                            attachments.push(item[key]);
+                        }
+                    });
+
+                    batchEmail.push({
+                        "to": item.email,
+                        "subject": item.subject,
+                        "body": emailBody,
+                        "attachments": attachments,
+                        "_header": item
+                    });
+                });
+
+                console.log("Attempting to send " + batchEmail.length + " emails, please wait...");
+                batchEmailer(batchEmail, emailCallback);
+            }
+        });
+
+    function emailCallback(successfulEmails, failedEmails) {
+        if (failedEmails.length == 0) {
+            console.log("All emails were sent successfully");
+
+            readline.question("Press enter to exit\n");
         } else {
-            var batchEmail = [];
+            const failedEmailAddresses = failedEmails.map(email => email.to);
+            console.log("Sending failed for: " + failedEmailAddresses);
 
-            headerJSON.forEach(function(item, index, allItems) {
-                var emailBody = emailBodyTemplate;
+            failedEmails.forEach(email => addFailedRecordInCSV(email._header));
 
-                var attachments = [];
-
-                Object.keys(item).forEach(key => {
-                    emailBody = emailBody.split("<" + key + ">").join(item[key]);
-
-                    if (key.startsWith("attachment")) {
-                        attachments.push(item[key]);
-                    }
-                });
-
-                batchEmail.push({
-                    "to": item.email,
-                    "subject": item.subject,
-                    "body": emailBody,
-                    "attachments": attachments,
-                    "_header": item
-                });
-            });
-
-            console.log("Attempting to send " + batchEmail.length + " emails, please wait...");
-            batchEmailer(batchEmail, emailCallback);
+            if (readline.question('Would you like to retry sending the failed ones? (Y/n) ').match(/[Yy]/)) {
+                intiateEmails(getNextRetryCSVFileName(), contentTemplate);
+            }
         }
-    });
-
-function emailCallback(successfulEmails, failedEmails) {
-    if (failedEmails.length == 0) {
-        console.log("All emails were sent successfully");
-    } else {
-        const failedEmailAddresses = failedEmails.map(email => email.to);
-        console.log("Sending failed for: " + failedEmailAddresses);
-
-        failedEmails.forEach(email => addFailedRecordInCSV(email._header));
     }
 
-    readline.question("Press enter to exit\n");
-}
-
-function getNextRetryCSVFileName() {
-    return headerCSV.substring(0, headerCSV.length - 4) + '-retry.csv';
-}
-
-function addFailedRecordInCSV(item) {
-    createFailedHeaderCSVFileIfNeeded(item);
-
-    var row = '';
-    Object.keys(item).forEach(key => {
-        row += item[key] + ',';
-    });
-
-    fs.appendFileSync(getNextRetryCSVFileName(),
-        '\n' + row.substring(0, row.length - 1),
-        'utf8');
-}
-
-function createFailedHeaderCSVFileIfNeeded(item) {
-    if (!fs.existsSync(getNextRetryCSVFileName())) {
-        createFailedHeaderCSVFile(item);
+    function getNextRetryCSVFileName() {
+        return headerCSV.substring(0, headerCSV.length - 4) + '-retry.csv';
     }
-}
 
-function createFailedHeaderCSVFile(item) {
-    var header = '';
+    function addFailedRecordInCSV(item) {
+        createFailedHeaderCSVFileIfNeeded(item);
+
+        var row = '';
         Object.keys(item).forEach(key => {
-        header += key + ',';
-    });
+            row += item[key] + ',';
+        });
 
-    fs.writeFileSync(getNextRetryCSVFileName(),
-        header.substring(0, header.length - 1),
-        'utf8');
+        fs.appendFileSync(getNextRetryCSVFileName(),
+            '\n' + row.substring(0, row.length - 1),
+            'utf8');
+    }
+
+    function createFailedHeaderCSVFileIfNeeded(item) {
+        if (!fs.existsSync(getNextRetryCSVFileName())) {
+            createFailedHeaderCSVFile(item);
+        }
+    }
+
+    function createFailedHeaderCSVFile(item) {
+        var header = '';
+            Object.keys(item).forEach(key => {
+            header += key + ',';
+        });
+
+        fs.writeFileSync(getNextRetryCSVFileName(),
+            header.substring(0, header.length - 1),
+            'utf8');
+    }
 }
